934bdf3923ac36f86ae2ef6e02c806a459a1f539
Marcos Guerrini <marcosguerrini@gmail.com>
2018-10-10 22:17:18 -0300

    Clase 13 completamos archivo ejemplos

--- 13_utilidades_string.js
+++ 13_utilidades_string.js
@@ -1,4 +1,34 @@
-var cadena = "Hola mundo"
-var cadena2 = "soy un string"
+var cadena1 = "Hola mundo Hamburguesa";
+var cadena2 = " soy una cadena";
 
+// Calcular el tamaño de una cadena
+resultado = cadena1.length;
 
+// Concatenar cadenas
+resultado = cadena1 + cadena2;
+resultado = cadena1.concat(cadena2);
+
+// Convertir cadena en mayusculas
+resultado = cadena1.toUpperCase();
+
+// Convertir a minusculas
+resultado = cadena1.toLowerCase();
+
+// Obtener el caracter ubicado en una posición que indicamos
+resultado = cadena1.charAt(2);
+
+// Obtener la primera posición encontrada de un caracter o cadena
+resultado = cadena1.indexOf("ola");
+
+// Obtenemos la última coincidencia
+resultado = cadena1.lastIndexOf("H");
+
+// Tomar una parte de una cadena
+resultado = cadena1.substring(2);
+
+resultado = cadena1.substring(2, 10);
+
+// Dividir cadena de texto
+var resultado = cadena1.split("");
+
+document.write(resultado);
